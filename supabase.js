08a00c563f3ed42aa1185eb903925e88868ba9ff
Du <y9kfcbr4mm@privaterelay.appleid.com>
2026-05-29 11:45:59 +0100

    Update Supabase client configuration
    Updated Supabase URL and key for client initialization.

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://ivyuzrbdjpncvjsnjybm.supabase.co/rest/v1/";
-const SUPABASE_KEY = "ivyuzrbdjpncvjsnjybm";
+const SUPABASE_URL = "https://ivyuzrbdjpncvjsnjybm.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2eXV6cmJkanBuY3Zqc25qeWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NzgwNjEsImV4cCI6MjA5NTU1NDA2MX0._70MBLXRgi69eeEezdQ3pD-BSQmCT_6zGA4hvIXJR8A";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
